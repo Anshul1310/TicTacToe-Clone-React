e08a7d347ab266ca1d5fbd0219542bd924fd85f9
Epiccode03 <huntinggamingtech@gmail.com>
2022-04-19 10:08:34 +0530

    commit7

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App(props) {
 
   const [gameState, setGameSate]=useState(initialState);
   const [isXturn, setTurn]=useState(false);
-  const winningPos=[[0,1,2], [0,3,6],[6,7,8],[2,5,8],[0,4,8],[2,4,6],[3,4,5],[1,4,7]];
 
 
   useEffect(()=>{
+      const winningPos=[[0,1,2], [0,3,6],[6,7,8],[2,5,8],[0,4,8],[2,4,6],[3,4,5],[1,4,7]];
+
         for(var i=0;i<winningPos.length;i++){
           const arr=winningPos[i];
           if(gameState[arr[0]]!=="" && gameState[arr[1]]!=="" && gameState[arr[0]]===gameState[arr[1]] && gameState[arr[1]]===gameState[arr[2]]){
@@ -19,7 +20,7 @@ function App(props) {
             alert(playerWon+" has won");
           }
     }
-  },[gameState, isXturn, winningPos]);
+  },[gameState, isXturn]);
 
   function updatePlayerState(index){
       let strings=Array.from(gameState);
